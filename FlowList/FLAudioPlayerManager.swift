1ce2291648683917862232ea5985c158e7c486b2
Daniel Rakhamimov <danielrak@me.com>
2014-12-21 11:05:57 -0500

    Song Playback

---
+++ FlowList/FLAudioPlayerManager.swift
@@ -0,0 +1,13 @@
+//
+//  FLAudioPlayerManager.swift
+//  FlowList
+//
+//  Created by Daniel on 12/20/14.
+//  Copyright (c) 2014 Daniel Rakhamimov. All rights reserved.
+//
+
+import Cocoa
+
+class FLAudioPlayerManager: NSObject {
+
+}
