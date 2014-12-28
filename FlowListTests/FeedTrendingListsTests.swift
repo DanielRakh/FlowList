b81ae31d196f5af7fac4cfdab96a9f34b05722a4
Daniel Rakhamimov <danielrak@me.com>
2014-12-28 14:21:38 -0500

    Tests: Create FeedTrendingListsTests

---
+++ FlowListTests/FeedTrendingListsTests.swift
@@ -0,0 +1,21 @@
+//
+//  FlowListTests.swift
+//  FlowListTests
+//
+//  Created by Daniel on 12/20/14.
+//  Copyright (c) 2014 Daniel Rakhamimov. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class FeedTrendingListTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    
+    
+}
