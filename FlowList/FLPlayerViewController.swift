64f6fb0995d6c9ed391e6e261aa3efca1a3f7cd5
Daniel Rakhamimov <danielrak@me.com>
2015-01-02 00:49:17 -0500

    Start Player Module

---
+++ FlowList/FLPlayerViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FLPlayerViewController.swift
+//  FlowList
+//
+//  Created by Daniel on 1/2/15.
+//  Copyright (c) 2015 Daniel Rakhamimov. All rights reserved.
+//
+
+import UIKit
+
+class FLPlayerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ FlowList/FLPlayerWireframe.swift
@@ -0,0 +1,13 @@
+//
+//  FLPlayerWireframe.swift
+//  FlowList
+//
+//  Created by Daniel on 1/2/15.
+//  Copyright (c) 2015 Daniel Rakhamimov. All rights reserved.
+//
+
+import UIKit
+
+class FLPlayerWireframe: NSObject {
+   
+}
